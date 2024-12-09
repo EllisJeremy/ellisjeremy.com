0f2c7bfd3333408ecf7c6fe717b8685e5d1fd8b0
Jeremy Ellis <jeremyellis173@gmail.com>
2024-12-09 16:03:19 -0500

    mathjs install

--- dist/logic.js
+++ dist/logic.js
@@ -1,4 +1,5 @@
-const element = document.getElementById("X11");
-if (element) {
-  element.textContent = "Updated Content";
-}
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mathjs_1 = require("mathjs");
+const result = (0, mathjs_1.evaluate)('2 + 3');
+console.log(result); // Should log: 5
---
+++ src/logic.ts
@@ -0,0 +1,4 @@
+import { evaluate } from 'mathjs';
+
+const result = evaluate('2 + 3');
+console.log(result); // Should log: 5
