3b5c43d8840fb7a6811f0d07d58fd88b0314001a
Jeremy Ellis <jeremyellis173@gmail.com>
2024-12-20 20:09:43 -0500

    moving to react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Static from './components/Static';
+import Dynamic from './components/Dynamic';
+
+function App() {
+  return (
+    <div>
+      <Static />
+      <Dynamic />
+    </div>
+  );
+}
+
+export default App;
