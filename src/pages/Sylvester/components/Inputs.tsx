6818bc66a35f94066b5f837efb10212677a95ee6
Jeremy Ellis <jeremyellis173@gmail.com>
2025-01-09 18:49:32 -0500

    adding matrix state

--- src/pages/Sylvester/components/Inputs.tsx
+++ src/pages/Sylvester/components/Inputs.tsx
@@ -3,7 +3,7 @@ import { inputsStore } from "../store"
 
 export default function Inputs() {
 
-  const {n, m, setN, setM} = inputsStore();
+  const {n, m, setN, setM, incrementN, decrementN, incrementM, decrementM} = inputsStore();
  
 
 	return(
@@ -17,16 +17,20 @@ export default function Inputs() {
 
 					<div className = "dimension-input-div">
 						<label className="dimension-label" htmlFor  = "dimension-n">
-							<span className="tnr2">n</span>&nbsp; = &nbsp;
+							<span className="tnr2">n</span>&nbsp;&nbsp; = &nbsp;
 						</label>
 						<input className="dimension-input" type="number" placeholder="max 9" value = {n} onChange={(e) => setN(e.currentTarget.valueAsNumber)} min="1" max="9" step ="1"></input>
+            <button className="dimension-input-button" onClick={decrementN}>-</button>
+            <button className="dimension-input-button" onClick={incrementN}>+</button>
 					</div>
 
 					<div className = "dimension-input-div">
 						<label className="dimension-label ">
-							<span className="tnr2">m</span> = &nbsp;
+							<span className="tnr2">m</span>&nbsp; = &nbsp;
 						</label>
 						<input className="dimension-input" type="number" placeholder="max 9" value = {m} onChange={(e) => setM(e.currentTarget.valueAsNumber)} min="1" max="9" step ="1"></input>
+            <button className="dimension-input-button" onClick={decrementM}>-</button>
+            <button className="dimension-input-button" onClick={incrementM}>+</button>
 					</div>
 
 				</div>
--- src/pages/Sylvester/store.ts
+++ src/pages/Sylvester/store.ts
@@ -7,21 +7,66 @@ export interface inputsStoreType {
   n: number;
   m: number;
   
-  setN: (age: number) => void;
-  setM: (age: number) => void;
+  setN: (n: number) => void;
+  setM: (m: number) => void;
+
+  incrementN: () => void;
+  decrementN: () => void;
+  incrementM: () => void;
+  decrementM: () => void;
 }
 
 //default vales and functions for m and n inputs 
-export const inputsStore = create<inputsStoreType>(
-  (set) => ({
+export const inputsStore = create<inputsStoreType>((set) => ({
     
     n: 2, 
     m: 2, 
 
     setN : (n: number) => set(() => ({n})),
     setM : (m: number) => set(() => ({m})),
+
+    incrementN: () => set((state) => {
+      if (state.n < 9){
+        return { n: state.n + 1 };
+      }
+      return state;
+     }),
+    decrementN: () => set((state) => {
+      if (state.n > 1) {
+        return { n: state.n - 1 }; 
+      }
+      return state; 
+    }),
+  
+    incrementM: () => set((state) => {
+      if (state.m < 9){
+        return { m: state.m + 1 };
+      }
+      return state;
+     }),
+    decrementM: () => set((state) => {
+      if (state.m > 1) {
+        return { m: state.m - 1 }; 
+      }
+      return state; 
+    }),
   })
 );
 
 
+export interface matricesStoreType {
+  
+  matrixA: number[][];
+  
+  setMatrixA: (matrixA: number[][]) => void;
+}
 
+export const matricesStore = create<matricesStoreType>(
+  (set) => ({
+
+    matrixA: [[0]],
+
+    setMatrixA : (matrixA: number[][]) => set(() => ({matrixA})),
+   
+  })
+);
