772053e235def4176a0c2827fac8880a65fe0c4b
Jeremy Ellis <jeremyellis173@gmail.com>
2025-01-19 00:37:15 -0500

    ready for host

--- src/pages/Home/components/Intro/Intro.tsx
+++ src/pages/Home/components/Intro/Intro.tsx
@@ -1,5 +1,11 @@
 import styles from "./Intro.module.css";
 import hokie from "../.././assets/hokie.png"
+import python from "../.././assets/python.svg"
+import java from "../.././assets/java.svg"
+import javascript from "../.././assets/javascript.png"
+import typescript from "../.././assets/typescript2.svg"
+import html from "../.././assets/html.svg"
+import css from "../.././assets/css.svg"
 
 export default function Intro() {
   
@@ -14,23 +20,34 @@ export default function Intro() {
         <div className={styles.nameDiv}>
           <h1 className={styles.name}>Jeremy Ellis</h1>
 
-            <div className={styles.subtitleDiv} onClick={virginiaTechLink}>
+            <div className={styles.subtitleDiv}>
               <h2 className={styles.subName}> CS student at &nbsp;</h2>
-              <div className={styles.hokieDiv}>
+              <div className={styles.VirginiaTechDiv} onClick={virginiaTechLink}>
                 <h2 className={styles.subName}><span className={styles.maroonSpan}> Virginia</span> <span className={styles.orangeSpan}>Tech</span> </h2>
-                <img className={styles.hokie} src={hokie}></img>
+                <div className={styles.hokieDiv}>
+                  <img className={styles.hokie} src={hokie}></img>
+                </div>
               </div>
             </div>
 
         </div>
 
         <div className={styles.aboutDiv}>
+          <p className={styles.about}>Welcome to my portfolio site! I am an aspiring software engineer who works with the following languages:</p>
+          
+          <div className={styles.languagesDiv}>
+            <img className={styles.languageImg} src={python}></img>
+            <img className={styles.languageImg} src={java}></img>
+            <img className={styles.languageImg} src={typescript}></img>
+            <img className={styles.languageImg} src={javascript}></img>
+            <img className={styles.languageImg} src={html}></img>
+            <img className={styles.languageImg} src={css}></img>
+          </div>
 
+          <p className={styles.project}>To check out any of the projects below, click on a title or image!</p>
 
         </div>
-          <p className={styles.about}>Welcome to my portfolio site! I am an aspiring software engineer in m</p>
-
-        </div>
+      </div>
     </>
   );
 }
