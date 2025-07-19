99fdf71dafee39677936148ed69811e387743d4f
Jeremy Ellis <jeremyellis173@gmail.com>
2025-07-19 16:53:14 -0400

    feat: ui changes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,3 @@
-
 import { Helmet } from "react-helmet";
 import styles from "./Home.module.css";
 import Intro from "./components/Intro/Intro";
@@ -6,22 +5,26 @@ import Project from "./components/Project/Project1";
 import Project2 from "./components/Project/Project2";
 
 export default function Home() {
-
   return (
     <>
-
       <Helmet>
         <body className={styles.home} />
         <meta name="viewport" content="width=device-width, initial-scale=.6" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fattern&display=swap"
+          rel="stylesheet"
+        />
       </Helmet>
-      <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
       <div className={styles.lightBar1} />
       <Intro />
       <Project />
       <Project2 />
       <div className={styles.lightBar2} />
-
     </>
   );
 }
--- src/pages/Home/components/Intro/Intro.tsx
+++ src/pages/Home/components/Intro/Intro.tsx
@@ -1,40 +1,47 @@
 import styles from "./Intro.module.css";
-import hokie from "../.././assets/hokie.png"
-import python from "../.././assets/python.png"
-import java from "../.././assets/java.svg"
-import javascript from "../.././assets/javascript.png"
-import typescript from "../.././assets/typescript2.svg"
-import html from "../.././assets/html.png"
-import css from "../.././assets/css.png"
+import hokie from "../.././assets/hokie.png";
+import python from "../.././assets/python.png";
+import java from "../.././assets/java.svg";
+import javascript from "../.././assets/javascript.png";
+import typescript from "../.././assets/typescript2.svg";
+import html from "../.././assets/html.png";
+import css from "../.././assets/css.png";
 
 export default function Intro() {
-  
   const virginiaTechLink = () => {
-    window.open('https://www.vt.edu/', '_blank'); 
+    window.open("https://www.vt.edu/", "_blank");
   };
 
   return (
     <>
-      
       <div className={styles.introDiv}>
         <div className={styles.nameDiv}>
-          <h1 className={styles.name}>Jeremy Ellis</h1>
+          <h1 className={styles.name}>
+            Jeremy
+            <br />
+            Ellis
+          </h1>
 
-            <div className={styles.subtitleDiv}>
-              <h2 className={styles.subName}> CS student at &nbsp;</h2>
-              <div className={styles.VirginiaTechDiv} onClick={virginiaTechLink}>
-                <h2 className={styles.subName}><span className={styles.maroonSpan}> Virginia</span> <span className={styles.orangeSpan}>Tech</span> </h2>
-                <div className={styles.hokieDiv}>
-                  <img className={styles.hokie} src={hokie}></img>
-                </div>
+          <div className={styles.subtitleDiv}>
+            <h2 className={styles.subName}> CS student at &nbsp;</h2>
+            <div className={styles.VirginiaTechDiv} onClick={virginiaTechLink}>
+              <h2 className={styles.subName}>
+                <span className={styles.maroonSpan}> Virginia</span>{" "}
+                <span className={styles.orangeSpan}>Tech</span>{" "}
+              </h2>
+              <div className={styles.hokieDiv}>
+                <img className={styles.hokie} src={hokie}></img>
               </div>
             </div>
-
+          </div>
         </div>
 
         <div className={styles.aboutDiv}>
-          <p className={styles.about}>Welcome to my portfolio site! I am an aspiring software engineer who works with the following languages:</p>
-          
+          <p className={styles.about}>
+            Welcome to my portfolio site! I am an aspiring software engineer who
+            works with the following languages:
+          </p>
+
           <div className={styles.languagesDiv}>
             <img className={styles.languageImg} src={python}></img>
             <img className={styles.languageImg} src={java}></img>
@@ -44,8 +51,9 @@ export default function Intro() {
             <img className={styles.languageImg} src={css}></img>
           </div>
 
-          <p className={styles.project}>To check out any of the projects below, click on a title or image!</p>
-
+          <p className={styles.project}>
+            To check out any of the projects below, click on a title or image!
+          </p>
         </div>
       </div>
     </>
