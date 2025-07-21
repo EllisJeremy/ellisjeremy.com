197324f7447e600b114d1df39acfb702027f7431
Jeremy Ellis <jeremyellis173@gmail.com>
2025-07-20 23:36:02 -0400

    feat: added footer

--- src/pages/Home/components/Left/Left.tsx
+++ src/pages/Home/components/Left/Left.tsx
@@ -1,6 +1,7 @@
 import styles from "./Left.module.css";
 import hokie from "../.././assets/hokie.png";
 import { homeStore } from "../../store";
+import { useRef } from "react";
 
 export default function Left() {
   const virginiaTechLink = () => {
@@ -18,52 +19,55 @@ export default function Left() {
 
   return (
     <>
-      <div className={styles.nameDiv}>
-        <h1 className={styles.name}>Jeremy Ellis</h1>
+      <header>
+        <div className={styles.nameDiv}>
+          <h1 className={styles.name}>Jeremy Ellis</h1>
 
-        <div className={styles.subtitleDiv}>
-          <h2 className={styles.subName}> CS student at &nbsp;</h2>
-          <div className={styles.VirginiaTechDiv} onClick={virginiaTechLink}>
-            <h2 className={styles.subName}>
-              <span className={styles.maroonSpan}> Virginia</span>{" "}
-              <span className={styles.orangeSpan}>Tech</span>{" "}
-            </h2>
-            <div className={styles.hokieDiv}>
-              <img className={styles.hokie} src={hokie}></img>
+          <div className={styles.subtitleDiv}>
+            <h2 className={styles.subName}> CS student at &nbsp;</h2>
+            <div className={styles.VirginiaTechDiv} onClick={virginiaTechLink}>
+              <h2 className={styles.subName}>
+                <span className={styles.maroonSpan}> Virginia</span>{" "}
+                <span className={styles.orangeSpan}>Tech</span>{" "}
+              </h2>
+              <div className={styles.hokieDiv}>
+                <img className={styles.hokie} src={hokie}></img>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={styles.sectionButtonDiv}>
-        <div
-          className={styles.slideOver}
-          style={{ top: topMap[section] || "0px" }}
-        />
-        <button
-          className={styles.sectionButton}
-          onClick={() => setSection("home")}
-        >
-          home
-        </button>
-        <button
-          className={styles.sectionButton}
-          onClick={() => setSection("work")}
-        >
-          work
-        </button>
-        <button
-          className={styles.sectionButton}
-          onClick={() => setSection("projects")}
-        >
-          projects
-        </button>
-        <button
-          className={styles.sectionButton}
-          onClick={() => setSection("school")}
-        >
-          school
-        </button>
-      </div>
+        <div className={styles.sectionButtonDiv}>
+          <div
+            className={styles.slideOver}
+            style={{ top: topMap[section] || "0px" }}
+          />
+          <button
+            className={styles.sectionButton}
+            onClick={() => setSection("home")}
+          >
+            home
+          </button>
+          <button
+            className={styles.sectionButton}
+            onClick={() => setSection("work")}
+          >
+            work
+          </button>
+          <button
+            className={styles.sectionButton}
+            onClick={() => setSection("projects")}
+          >
+            projects
+          </button>
+          <button
+            className={styles.sectionButton}
+            onClick={() => setSection("school")}
+          >
+            school
+          </button>
+        </div>
+      </header>
+      <footer>text</footer>
     </>
   );
 }
