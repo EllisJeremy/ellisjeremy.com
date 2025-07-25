19aa6c7a2ade64ed3e34c1838430ec14363b0254
Jeremy Ellis <jeremyellis173@gmail.com>
2025-07-24 20:45:36 -0400

    fix: va tech logo

--- src/pages/Home/components/Left/Left.tsx
+++ src/pages/Home/components/Left/Left.tsx
@@ -50,11 +50,11 @@ export default function Left() {
           <h1 className={styles.name}>Jeremy Ellis</h1>
 
           <div className={styles.subtitleDiv}>
-            <h2 className={styles.subName}> CS student at &nbsp;</h2>
+            <h2 className={styles.subName}> CS student at&nbsp;</h2>
             <div className={styles.VirginiaTechDiv} onClick={virginiaTechLink}>
               <h2 className={styles.subName}>
-                <span className={styles.maroonSpan}> Virginia</span>
-                <span className={styles.orangeSpan}>Tech</span>
+                <span className={styles.maroonSpan}>Virginia</span>
+                <span className={styles.orangeSpan}> Tech</span>
               </h2>
               <div className={styles.hokieDiv}>
                 <img className={styles.hokie} src={hokie}></img>
