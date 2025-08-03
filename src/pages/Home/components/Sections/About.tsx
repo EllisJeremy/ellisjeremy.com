5a5a96d9a32c0a0cd8be6fbe4b6bdd508f26493a
Jeremy Ellis <jeremyellis173@gmail.com>
2025-08-03 17:17:27 -0400

    fix: gitignore

--- src/pages/Home/components/Sections/About.tsx
+++ src/pages/Home/components/Sections/About.tsx
@@ -4,7 +4,11 @@ export default function About() {
   return (
     <div className={styles.worksCitedDiv}>
       <div className={styles.experienceDiv}>
-        <div className={styles.aboutDiv}></div>
+        <div className={styles.aboutDiv}>
+          Welcome to my portfolio site!
+          <br />
+          <br />I am
+        </div>
       </div>
     </div>
   );
